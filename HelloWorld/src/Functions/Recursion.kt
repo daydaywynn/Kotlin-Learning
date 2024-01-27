dcdeb37684017d8bda854a4526b94d9c8b1eef37
dayda <dayda@LAPTOP-O6TLUMO7>
2024-01-27 12:04:08 -0600

    Classes and Objects (The beginning of OOP)

--- HelloWorld/src/Functions/Recursion.kt
+++ HelloWorld/src/Functions/Recursion.kt
@@ -6,8 +6,14 @@ fun main() {
 
     result = factorial(number)
     println("Factorial of $number = $result")
+	
+	val result_ = factorials(number)
+    println("Factorial of $number = $result_")
 }
 //A regular recursive function
 fun factorial(n: Int): Long {
     return if (n == 1) n.toLong() else n*factorial(n-1)
 }
+
+//A rewritten recursive function
+fun factorials(n: Int): Long = if (n == 1) n.toLong() else n*factorial(n-1)
